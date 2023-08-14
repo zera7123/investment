54ae192703aa585d7fdb15f9799dd4d780411513
zera7123 <planet.home.0826@gmail.com>
2023-08-15 08:48:33 +0900

    sell_add

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
     formatted_data = []
     p_and_l_total = 0
     for row in data:
-        if row[12] == 1:
+        if row[13] == 1:
             formatted_row = []
             for i, x in enumerate(row):
                 if i == 3:
@@ -87,7 +87,7 @@ def c_price():
     data = cur.fetchall()
     
     for row in data:
-        if row[12] == 1:
+        if row[13] == 1:
             for i, x in enumerate(row):
                 if i == 11:
                     current_id = row[0]
@@ -129,7 +129,7 @@ def total():
     formatted_data = []
     p_and_l_total = 0
     for row in data:
-        if row[12] == 1:
+        if row[13] == 1:
             formatted_row = []
             for i, x in enumerate(row):
                 if i == 3:
@@ -233,6 +233,7 @@ def data():
                 formatted_data.append(p_and_l_for)
             else:
                 formatted_data.append(p_and_l)
+            formatted_data.append(row[12])
                
     return render_template('data.html',data = formatted_data, today = today)
 
@@ -252,6 +253,10 @@ def add_buy():
         
         t_number = int(p_number) + int(b_number)
         t_price = ((Decimal(p_price)*Decimal(p_number))+(Decimal(b_price)*Decimal(b_number)))/Decimal(t_number)
+        if t_price is not None:
+            t_price = format(t_price,',')
+        else:
+            t_price = 0
         
         cur = mysql.connection.cursor()
         cur.execute("UPDATE mytable SET b_price = %s, b_number = %s, b_date = %s, b_reason = %s  WHERE id = %s", (t_price, t_number, b_date, b_reason, id_number))
@@ -262,18 +267,41 @@ def add_buy():
 
 @app.route('/sell', methods=['POST'])
 def sell():
+    id_number = request.args.get('arg0')
+    b_price = request.arges.get('arg1')
+    c_number = request.args.get('arg2')
+    c_t_pl = request.args.get('arg3')
+       
     if request.method == 'POST':
-        code = request.form['code']
-        name = request.form['name']
-        b_price = request.form['b_price']
-        b_number = request.form['b_number']
-        b_date = request.form['b_date']
-        b_reason = request.form['b_reason']
-        status = 1
+        # code = request.form['code']
+        # name = request.form['name']
+        s_price = request.form['s_price']
+        s_number = request.form['s_number']
+        s_date = request.form['s_date']
+        s_reason = request.form['s_reason']
+        
+        if s_price is not None:
+            s_price_for = format(s_price,',')
+        else:
+            s_price_for = 0
+        
+        r_number = int(c_number) - int(s_number)
+        if r_number == 0:
+            status = 0
+        else:
+            status = 1
+            
+        pl = (Decimal(s_price)-Decimal(b_price))*Decimal(s_number)
+        t_pl = Decimal(c_t_pl)+Decimal(pl)
+        if t_pl is not None:
+            t_pl = format(t_pl,',')
+        else:
+            t_pl = 0
+               
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO mytable(code, name, b_price, b_number, b_date, b_reason, status) VALUES (%s, %s, %s, %s, %s, %s, %s)", (code, name, b_price, b_number, b_date, b_reason, status))
+        cur.execute("UPDATE mytable SET b_number = %s, s_price = %s, s_number = %s, s_date = %s, s_reason = %s, total_pl = %s, status = %s  WHERE id = %s", (r_number, s_price_for, s_number, s_date, s_reason, t_pl, status, id_number))
         mysql.connection.commit()
-        cur.close()      
+        cur.close()
     return redirect(url_for('index'))
 
 
